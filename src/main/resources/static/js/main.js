b822835505a507ee53b71cbdfa3fccd9b2a1b230
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-06-12 16:34:48 -0500

    feat: home page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,4 +6,17 @@
         e.preventDefault();
         logoutForm.submit();
     });
+
+    let video = document.getElementById("myVideo");
+    let btn = document.getElementById("myBtn");
+
+    function myFunction() {
+        if (video.paused) {
+            video.play();
+            btn.innerHTML = "Pause";
+        } else {
+            video.pause();
+            btn.innerHTML = "Play";
+        }
+    }
 }
